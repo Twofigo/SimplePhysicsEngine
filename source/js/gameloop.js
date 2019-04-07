08cd3e10ef83c1aab6121bf03207737501f587cc
August Baaz <august.baaz@gmail.com>
2019-04-07 18:31:03 +0200

    removed old code from gameloop

--- source/js/gameloop.js
+++ source/js/gameloop.js
@@ -72,43 +72,6 @@ function setup()
     this.obj.geometry = new physics.Geometry();
     var r = new physics.Rope()
 
-    var tracker = new physics.InputTracker();
-    tracker.set(document.getElementById("gameboard"));
-    tracker.enable();
-
-
-    //g = new Grabber();
-    //tracker.addListener("move", function(d){g.move(d)});
-    //tracker.addListener("m1", function(d){g.grabAndDrop(d)});
-
-    ins.start();
-}
-
-var Grabber = function(){
-    this.obj = new physics.RigidBody();
-    this.obj.geometry = new physics.Geometry();
-    this.obj.geometry.compile(true);
-    this.constraint = new physics.Rope(this.obj, new physics.Vector(), this.obj, new physics.Vector(), 0);
-    ins.add(this.constraint);
-}
-Grabber.prototype.grabAndDrop = function(data){
-    if(data.state)
-    {
-        var cordinate = ins.coordinateConvert(data.position);
-        var body = ins.bodyAtPoint(cordinate, data.timeStamp);
-        if(!body) return;
-        this.constraint.bodyB = body;
-        this.constraint.positionB = cordinate.subtract(body.getPosition(data.timeStamp)).rotate(-body.getAngle(data.timeStamp))
-    }
-    if(!data.state)
-    {
-        this.constraint.bodyB = this.obj;
-        this.constraint.positionB.set();
-    }
-}
-Grabber.prototype.move = function(data){
-    var cordinate = ins.coordinateConvert(data.position);
-    this.obj.setTimeStamp(data.timeStamp);
-    this.obj.setVelocity(data.velocity.scale(0.5).scale(1/ins.zoom), data.timeStamp);
-    this.obj.setPosition(cordinate, data.timeStamp);
+    view = scene.createView(document.getElementById("gameboard"));
+    scene.start();
 }
