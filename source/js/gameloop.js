95653e9a043f09b6e229bf0cda0fb4dd28906f08
August Baaz <august.baaz@gmail.com>
2019-04-07 18:31:29 +0200

    cleanup gameloop

--- source/js/gameloop.js
+++ source/js/gameloop.js
@@ -1,7 +1,8 @@
-var ins;;
+var view
+var scene
 function setup()
 {
-    ins = new physics.Scene()
+    scene = physics.createScene();
 
     var geo = new physics.Geometry();
     geo.setVertices([
@@ -16,12 +17,12 @@ function setup()
     obj.geometry = geo;
     obj.setStartPosition(0,0,1);
     obj.setStartVelocity(70,0,0);
-    ins.add(obj);
+    scene.add(obj);
 
     var obj = new physics.RigidBody("Jeff")
     obj.geometry = geo;
     obj.setStartPosition(100,0,0);
-    ins.add(obj);
+    scene.add(obj);
 
     // floor;
     var geo = new physics.Geometry();
@@ -31,18 +32,19 @@ function setup()
     {x:800,y:100},
     {x:-800,y:100},
     ]);
-    geo.compile(true);
+    geo.compile();
+    geo.freeze(true);
 
     var obj = new physics.RigidBody("floor");
     obj.geometry = geo;
     obj.setStartPosition(0,400);
-    ins.add(obj);
+    scene.add(obj);
 
     // roof
     var obj = new physics.RigidBody("roof");
     obj.geometry = geo;
     obj.setStartPosition(0,-400);
-    ins.add(obj);
+    scene.add(obj);
 
     // left wall
     var geo = new physics.Geometry();
@@ -52,21 +54,22 @@ function setup()
     {x:100,y:800},
     {x:-100,y:800},
     ]);
-    geo.compile(true);
+    geo.compile();
+    geo.freeze(true);
 
     var obj = new physics.RigidBody("left");
     obj.geometry = geo;
     obj.setStartPosition(-500,0);
-    ins.add(obj);
+    scene.add(obj);
 
     // right wall
     var obj = new physics.RigidBody("right");
     obj.geometry = geo;
     obj.setStartPosition(500,0);
-    ins.add(obj);
+    scene.add(obj);
 
-    ins.gravity.y=300;
-    ins.setup(document.getElementById("gameboard"));
+    scene.gravity.y=300;
+    scene.setup(document.getElementById("gameboard"));
 
     this.obj = new physics.RigidBody();
     this.obj.geometry = new physics.Geometry();
